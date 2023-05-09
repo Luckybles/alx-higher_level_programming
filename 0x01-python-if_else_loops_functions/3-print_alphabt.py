9d62651d0a85f1d4b542a51290ad2a6f4722f21e
luckybles <blesssmith169@gmail.com>
2023-05-09 15:25:22 -0700

    still alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for d in range(ord('a'), ord('z') + 1):
-    if d != ord('e') and d != ord('q'):
-        print("{:d}".format(d), end="")
+for c in range(ord('a'), ord('z') + 1):
+    if c != ord('e') and c!= ord('q'):
+        print("{:c}".format(c), end="")
