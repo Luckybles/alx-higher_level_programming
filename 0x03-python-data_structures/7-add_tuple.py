6f63d5f12fb23de0a46e27c0a3962ee3ee025191
luckybles <blesssmith169@gmail.com>
2023-05-15 01:34:42 -0700

    tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,5 +3,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     update_tuple =()
     tuple_a += (0, 0)
     tuple_b += (0, 0)
-    update_tuple = tuple_a[0] + tuple_b[0] + tuple_a[1] + tuple_b[1]
+    update_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     return update_tuple
