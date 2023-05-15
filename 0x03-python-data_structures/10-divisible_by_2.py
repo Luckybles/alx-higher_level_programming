94caec7956629e09b5e994415642a7a66084be2a
luckybles <blesssmith169@gmail.com>
2023-05-15 02:01:36 -0700

    divisible

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    update_list = []
+    for items in range(len(my_list)):
+        if my_list[items] % 2 == 0:
+            update_list.append(True)
+        else:
+            update_list.append(False)
+        return update_list
